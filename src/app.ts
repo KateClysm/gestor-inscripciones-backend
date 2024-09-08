c2c04dfe1cedb258a68ff76b960a2e28c87754f6
Katerina <kate.clysm@gmail.com>
2024-09-08 18:54:27 -0300

    Cambio en app y database

--- src/app.ts
+++ src/app.ts
@@ -3,18 +3,21 @@ import config from './config';
 import morgan from 'morgan';
 import cors from 'cors';
 import projectRoutes from './routes/projectRoutes';
-import userRoutes from './routes/userRoutes'
+import userRoutes from './routes/userRoutes';
 import entityRoutes from './routes/entityRoutes';
 
 const app = express();
 
+// Configurar el puerto
 app.set('port', config.PORT);
 
+// Middlewares
 app.use(morgan('dev'));
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 
+// Rutas
 app.use('/api/projects', projectRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/entities', entityRoutes);
--- src/database.ts
+++ src/database.ts
@@ -1,20 +1,13 @@
 import mongoose from 'mongoose';
 import config from './config';
 
-const connectDB = async () => {
+(async () => {
     try {
         const db = await mongoose.connect(
-            `mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`,
-            {
-                user: config.MONGO_USER,
-                pass: config.MONGO_PASSWORD,
-            }
+            `mongodb://${config.MONGO_HOST}/${config.MONGO_DATABASE}`
         );
         console.log('Database is connected to:', db.connection.name);
     } catch (error) {
         console.error('Database connection error:', error);
-        process.exit(1); // Detiene la aplicación si falla la conexión
     }
-};
-
-export default connectDB;
+})();
